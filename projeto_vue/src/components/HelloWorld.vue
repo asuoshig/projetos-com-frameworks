39c9578e7cb928c1ee46c7830eb537012a309062
geovana Ferreira de Jesus Sousa <gihsousa205@gmail.com>
2024-06-20 14:53:19 -0300

    teste-projeto vue com tailwind

---
+++ projeto_vue/src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="text-4xl text-blue-600 text-center p-30 m-30">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: {
+      type: String,
+      default: 'Hello World!'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.hello {
+  color: blue;
+}
+</style>
